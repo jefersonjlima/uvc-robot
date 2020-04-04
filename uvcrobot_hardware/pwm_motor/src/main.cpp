930687c7f8fd5b710fb3dfd3a21dea7cf85d91b5
Jeferson Lima <jefersonjl82@gmail.com>
2020-04-04 02:08:53 -0300

    bno055 odometry

--- uvcrobot_hardware/pwm_motor/src/main.cpp
+++ uvcrobot_hardware/pwm_motor/src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -7,14 +6,16 @@
 // ros libraries
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
-#include <geometry_msgs/Pose2D.h>
+#include <ros/time.h>
+#include <geometry_msgs/TransformStamped.h>
 // control
 #include "control.h"
 // #define DEBUG
+#define Adafruit_BNO055_ENABLE
 
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
-
+#ifdef Adafruit_BNO055_ENABLE
+  #define BNO055_SAMPLERATE_DELAY_MS (100)
+#endif
 #define LOOPTIME 100
 #define FORWARD 1
 #define REVERSE 0
@@ -27,102 +28,63 @@ const int PIN_L_PWM = 3;
 const int PIN_R_IN3 = 7;
 const int PIN_R_IN4 = 8;
 const int PIN_R_PWM = 9;
-
-
-ros::NodeHandle nh;
 int reverse = 0;
 float throttle = 0; 
 float angle = 0; 
 
+#ifdef Adafruit_BNO055_ENABLE
+  Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
+#endif
+
 void ControlRightMotor(uint8_t dir, uint8_t pwn);
 void ControlLeftMotor(uint8_t dir, uint8_t pwn);
+#ifdef Adafruit_BNO055_ENABLE
+  void Adafruit_BNO055_Details(void);
+#endif
+ros::NodeHandle nh;
+void handle_cmd( const geometry_msgs::Twist& twist);
 
-void cmdVelCB( const geometry_msgs::Twist& twist)
-{
-  throttle = twist.linear.x; 
-  angle = twist.angular.z; 
-
-  if (twist.linear.x <= 0)
-  {
-    ControlRightMotor(FORWARD, (uint8_t)abs(throttle));
-    ControlLeftMotor(FORWARD, (uint8_t)abs(throttle));
-  }
-  else if (twist.linear.x > 0){
-    ControlRightMotor(REVERSE, (uint8_t)throttle);
-    ControlLeftMotor(REVERSE, (uint8_t)throttle);
-  }
-
-  #ifdef DEBUG
-    Serial.println(throttle);
-  #endif
-}
-
-ros::Subscriber<geometry_msgs::Twist> subCmdVel("/uvcrobot/cmd_vel", cmdVelCB);
-
-//PWM TEST
-int count = 0;
+ros::Subscriber<geometry_msgs::Twist> cmd_vel("uvcrobot/cmd_vel", handle_cmd);
+#ifdef Adafruit_BNO055_ENABLE
+  geometry_msgs::TransformStamped pose_msg;
+  ros::Publisher pub_pose("uvcrobot/imu", &pose_msg);
+#endif
 pid_controller pid_z;
 
 void ControlRightMotor(uint8_t dir, uint8_t pwn);
 void ControlLeftMotor(uint8_t dir, uint8_t pwn);
 
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print("Sensor:       ");
-  Serial.println(sensor.name);
-  Serial.print("Driver Ver:   ");
-  Serial.println(sensor.version);
-  Serial.print("Unique ID:    ");
-  Serial.println(sensor.sensor_id);
-  Serial.print("Max Value:    ");
-  Serial.print(sensor.max_value);
-  Serial.println(" xxx");
-  Serial.print("Min Value:    ");
-  Serial.print(sensor.min_value);
-  Serial.println(" xxx");
-  Serial.print("Resolution:   ");
-  Serial.print(sensor.resolution);
-  Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
 
 void setup()
 {
-  Serial.begin(9600);
-  #ifdef DEBUG
-    Serial.println("Orientation Sensor Test");
-    Serial.println("");]
-  #endif
-
-  /* Initialise the sensor */
-
-  if (!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
+  #ifdef Adafruit_BNO055_ENABLE
+    Serial.begin(9600);
     #ifdef DEBUG
-      Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+      Serial.println("Orientation Sensor Test");
+      Serial.println("");]
     #endif
-    while (1);
-  }
-  else
-  {
+    /* Initialise the sensor */
+    if (!bno.begin())
+    {
+      /* There was a problem detecting the BNO055 ... check your connections */
+      #ifdef DEBUG
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+      #endif
+      while (1);
+    }
+    else
+    {
+      #ifdef DEBUG
+        Serial.println("OK BNO055!");
+      #endif
+    }
+    delay(1000);
+    /* Use external crystal for better accuracy */
+    bno.setExtCrystalUse(true);
+    /* Display some basic information on this sensor */
     #ifdef DEBUG
-      Serial.println("OK BNO055!");
+      Adafruit_BNO055_Details();
     #endif
-
-  }
-
-  delay(1000);
-  /* Use external crystal for better accuracy */
-  bno.setExtCrystalUse(true);
-  /* Display some basic information on this sensor */
-  #ifdef DEBUG
-    displaySensorDetails();
   #endif
   //Define os pinos como saida
   pinMode(PIN_L_PWM, OUTPUT);
@@ -132,18 +94,39 @@ void setup()
   pinMode(PIN_R_IN3, OUTPUT);
   pinMode(PIN_R_IN4, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  
   #ifndef DEBUG
     nh.initNode();
-    nh.subscribe(subCmdVel);
+    #ifdef Adafruit_BNO055_ENABLE
+      nh.advertise(pub_pose);
+    #endif
+    nh.subscribe(cmd_vel);
   #endif
 }
 
 void loop()
 {
-  #ifndef DEBUG
+    //Sampling period
+  if ((millis() - lastMilli) >= LOOPTIME)
+  {
+    #ifdef Adafruit_BNO055_ENABLE
+      // Get linear acceleration data from seneor
+      imu::Vector<3> linearaccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+      // Get quatrnion data from sensor 
+      imu::Quaternion quat = bno.getQuat();
+      //position data   
+      pose_msg.transform.translation.x = linearaccel.x();
+      pose_msg.transform.translation.y = linearaccel.y();
+      pose_msg.transform.translation.z = linearaccel.z();
+      // orientation data
+      pose_msg.transform.rotation.x = quat.x();
+      pose_msg.transform.rotation.y = quat.y();
+      pose_msg.transform.rotation.z = quat.z(); 
+      pose_msg.transform.rotation.w = quat.w();
+      //pose_msg.header.stamp = nh.now();
+      pub_pose.publish(&pose_msg);
+    #endif
     nh.spinOnce();
-  #endif
+  }
 }
 
 //Control function - Right Motors
@@ -176,4 +159,52 @@ void ControlLeftMotor(uint8_t dir, uint8_t pwn)
     digitalWrite(PIN_R_IN4, LOW);
   }
   analogWrite(PIN_R_PWM, pwn);
+}
+
+#ifdef Adafruit_BNO055_ENABLE
+  void Adafruit_BNO055_Details(void)
+  {
+    
+    sensor_t sensor;
+    bno.getSensor(&sensor);
+    Serial.println("------------------------------------");
+    Serial.print("Sensor:       ");
+    Serial.println(sensor.name);
+    Serial.print("Driver Ver:   ");
+    Serial.println(sensor.version);
+    Serial.print("Unique ID:    ");
+    Serial.println(sensor.sensor_id);
+    Serial.print("Max Value:    ");
+    Serial.print(sensor.max_value);
+    Serial.println(" xxx");
+    Serial.print("Min Value:    ");
+    Serial.print(sensor.min_value);
+    Serial.println(" xxx");
+    Serial.print("Resolution:   ");
+    Serial.print(sensor.resolution);
+    Serial.println(" xxx");
+    Serial.println("------------------------------------");
+    Serial.println("");
+    delay(500);
+  }
+#endif
+
+void handle_cmd( const geometry_msgs::Twist& twist)
+{
+  throttle = twist.linear.x; 
+  angle = twist.angular.z; 
+
+  if (twist.linear.x <= 0)
+  {
+    ControlRightMotor(FORWARD, (uint8_t)abs(throttle));
+    ControlLeftMotor(FORWARD, (uint8_t)abs(throttle));
+  }
+  else if (twist.linear.x > 0){
+    ControlRightMotor(REVERSE, (uint8_t)throttle);
+    ControlLeftMotor(REVERSE, (uint8_t)throttle);
+  }
+
+  #ifdef DEBUG
+    Serial.println(throttle);
+  #endif
 }
